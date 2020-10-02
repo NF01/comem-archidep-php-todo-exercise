965ffecf41f1ca70f406942c917f131945e88e37
NF01 <NF01@NF01>
2020-10-02 17:01:07 +0200

    Change DB_password

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
 define('BASE_URL', '/');
 define('DB_USER', 'todolist');
-define('DB_PASS', 'chAngeMeN0w!');
+define('DB_PASS', 'extestGit2020');
 define('DB_NAME', 'todolist');
 define('DB_HOST', '127.0.0.1');
 define('DB_PORT', '3306');
@@ -131,7 +131,7 @@ $items = $db->query($selectQuery);
             <form action='<?= BASE_URL ?>' method='post'>
               <input type='hidden' name='id' value='<?= $item['id'] ?>' />
 
-              <div class='btn-group btn-group-sm'</div>
+              <div class='btn-group btn-group-sm'>
 
                 <!-- Todo item toggle button -->
                 <button type='submit' name='action' value='toggle' class='btn btn-primary'>
